fd9e2d079f76eaa1be106fc32a852adb0b4fa139
ryouze <98982999+ryouze@users.noreply.github.com>
2025-01-05 00:37:32 +0100

    Add relative screen positions.

--- src/core/settings/screen.hpp
+++ src/core/settings/screen.hpp
@@ -6,6 +6,8 @@
 
 #pragma once
 
+#include <SFML/System/Vector2.hpp>
+
 namespace core::settings::screen {
 
 /**
@@ -17,5 +19,15 @@ constexpr unsigned int WIDTH = 800u;
  * @brief Default window height in pixels.
  */
 constexpr unsigned int HEIGHT = 600u;
+
+constexpr sf::Vector2f TOP_LEFT = {0.0f, 0.0f};
+constexpr sf::Vector2f TOP_CENTER = {WIDTH / 2.0f, 0.0f};
+constexpr sf::Vector2f TOP_RIGHT = {WIDTH, 0.0f};
+constexpr sf::Vector2f CENTER_LEFT = {0.0f, HEIGHT / 2.0f};
+constexpr sf::Vector2f CENTER = {WIDTH / 2.0f, HEIGHT / 2.0f};
+constexpr sf::Vector2f CENTER_RIGHT = {WIDTH, HEIGHT / 2.0f};
+constexpr sf::Vector2f BOTTOM_LEFT = {0.0f, HEIGHT};
+constexpr sf::Vector2f BOTTOM_CENTER = {WIDTH / 2.0f, HEIGHT};
+constexpr sf::Vector2f BOTTOM_RIGHT = {WIDTH, HEIGHT};
 
 }  // namespace core::settings::screen
