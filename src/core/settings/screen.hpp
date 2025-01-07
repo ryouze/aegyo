d154fc529ba7b645c913dd5e2b704c20d1319502
ryouze <98982999+ryouze@users.noreply.github.com>
2025-01-07 01:16:53 +0100

    Comment-out unused variables.

--- src/core/settings/screen.hpp
+++ src/core/settings/screen.hpp
@@ -25,44 +25,44 @@ constexpr unsigned int HEIGHT = 600u;
  */
 constexpr sf::Vector2f TOP_LEFT = {0.0f, 0.0f};
 
-/**
- * @brief Top-center of the screen.
- */
-constexpr sf::Vector2f TOP_CENTER = {WIDTH / 2.0f, 0.0f};
+// /**
+//  * @brief Top-center of the screen.
+//  */
+// constexpr sf::Vector2f TOP_CENTER = {WIDTH / 2.0f, 0.0f};
 
-/**
- * @brief Top-right corner of the screen.
- */
-constexpr sf::Vector2f TOP_RIGHT = {WIDTH, 0.0f};
+// /**
+//  * @brief Top-right corner of the screen.
+//  */
+// constexpr sf::Vector2f TOP_RIGHT = {WIDTH, 0.0f};
 
-/**
- * @brief Center-left of the screen.
- */
-constexpr sf::Vector2f CENTER_LEFT = {0.0f, HEIGHT / 2.0f};
+// /**
+//  * @brief Center-left of the screen.
+//  */
+// constexpr sf::Vector2f CENTER_LEFT = {0.0f, HEIGHT / 2.0f};
 
 /**
  * @brief Center of the screen.
  */
 constexpr sf::Vector2f CENTER = {WIDTH / 2.0f, HEIGHT / 2.0f};
 
-/**
- * @brief Center-right of the screen.
- */
-constexpr sf::Vector2f CENTER_RIGHT = {WIDTH, HEIGHT / 2.0f};
+// /**
+//  * @brief Center-right of the screen.
+//  */
+// constexpr sf::Vector2f CENTER_RIGHT = {WIDTH, HEIGHT / 2.0f};
 
-/**
- * @brief Bottom-left corner of the screen.
- */
-constexpr sf::Vector2f BOTTOM_LEFT = {0.0f, HEIGHT};
+// /**
+//  * @brief Bottom-left corner of the screen.
+//  */
+// constexpr sf::Vector2f BOTTOM_LEFT = {0.0f, HEIGHT};
 
-/**
- * @brief Bottom-center of the screen.
- */
-constexpr sf::Vector2f BOTTOM_CENTER = {WIDTH / 2.0f, HEIGHT};
+// /**
+//  * @brief Bottom-center of the screen.
+//  */
+// constexpr sf::Vector2f BOTTOM_CENTER = {WIDTH / 2.0f, HEIGHT};
 
-/**
- * @brief Bottom-right corner of the screen.
- */
-constexpr sf::Vector2f BOTTOM_RIGHT = {WIDTH, HEIGHT};
+// /**
+//  * @brief Bottom-right corner of the screen.
+//  */
+// constexpr sf::Vector2f BOTTOM_RIGHT = {WIDTH, HEIGHT};
 
 }  // namespace core::settings::screen
