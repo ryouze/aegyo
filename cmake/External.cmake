438559d11719cd4b2f433471478de8abc2255e6e
ryouze <98982999+ryouze@users.noreply.github.com>
2025-01-09 18:28:07 +0100

    Remove "[INFO]" prefixes from log messages.

--- cmake/External.cmake
+++ cmake/External.cmake
@@ -2,7 +2,7 @@ include(FetchContent)
 
 function(fetch_and_link_external_dependencies target)
   if(NOT TARGET ${target})
-    message(FATAL_ERROR "[ERROR] Target '${target}' does not exist. Cannot fetch and link dependencies.")
+    message(FATAL_ERROR "Target '${target}' does not exist. Cannot fetch and link dependencies.")
   endif()
 
   set(FETCHCONTENT_UPDATES_DISCONNECTED ON)
@@ -41,5 +41,5 @@ function(fetch_and_link_external_dependencies target)
     target_link_libraries(${target} PUBLIC SFML::Main)
   endif()
 
-  message(STATUS "[INFO] Linked dependencies 'fmt' and 'sfml' to target '${target}'.")
+  message(STATUS "Linked dependencies 'fmt' and 'sfml' to target '${target}'.")
 endfunction()
